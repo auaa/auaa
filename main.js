efff617b2b915c77915a4411d1ee8818113123a7
糯米 <qiuyungang@goboo.com>
2022-01-20 16:14:34 +0800

    1

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import './style.css'
 
+console.log('xxxxxxx');
 document.querySelector('#app').innerHTML = `
   <h1>Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
