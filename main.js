99bffaa86b2fbade1044cbcf8f23ae2dcd9fab6a
糯米 <qiuyungang@goboo.com>
2022-01-20 16:32:36 +0800

    env

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 import './style.css'
 
-console.log('xxxxxxx');
 document.querySelector('#app').innerHTML = `
   <h1>Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@
 const config = {
     base: '/auaa/'
 }
-
 export default config;
